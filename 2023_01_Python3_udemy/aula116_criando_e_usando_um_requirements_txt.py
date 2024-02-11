265774e962c55b1c1fa5547f4cdfd010a2a3f849
julliopereira <julliopereira@gmail.com>
2024-02-10 23:05:39 -0300

    Udemy Python 119 - abertura de arquivos

---
+++ 2023_01_Python3_udemy/aula116_criando_e_usando_um_requirements_txt.py
@@ -0,0 +1,30 @@
+# Ambientes virtuais em Python (venv)
+# Um ambiente virtual carrega toda a sua instalacao 
+# do Python para uma pasta no caminho escolhido.
+# Ao ativar um ambiente virtual, a instalacao do
+# ambiente virtual sera usada.
+# venv e o modulo que vamos usar para
+# criar ambientes virtuais.
+# Voce pode dar o nome que preferir para um 
+# ambiente virtual, mas os mais comuns sao:
+# venv, env .venv .env
+
+# ------------------------------------------------------------------------------------------
+
+# GERAR o arquivo requirements.txt
+# (mandadorio estar no ambiente virtual venv)
+
+# pip freeze > requirements.txt
+
+# jogar para o github
+
+# ------------------------------------------------------------------------------------------
+# Apos CLONAR o repositorio em outra maquina deve recuperar os modulos do ambiente virtual 
+
+# 1. clonar (virá o arquivo requirements.txt junto)
+# 2. crie um ambiente virtual novo 
+    # python3 -m venv venv
+# 3. instalar desde o arquivo os modulos necessarios
+    # pip install -r requirements.txt
+
+
---
+++ 2023_01_Python3_udemy/aula116_venv_ambientes_virtuais.py
@@ -0,0 +1,23 @@
+# Ambientes virtuais em Python (venv)
+# Um ambiente virtual carrega toda a sua instalacao 
+# do Python para uma pasta no caminho escolhido.
+# Ao ativar um ambiente virtual, a instalacao do
+# ambiente virtual sera usada.
+# venv e o modulo que vamos usar para
+# criar ambientes virtuais.
+# Voce pode dar o nome que preferir para um 
+# ambiente virtual, mas os mais comuns sao:
+# venv, env .venv .env
+
+
+# no python3.10 (hoje) tive que instalar venv
+# apt install python3-venv python3-pip 
+
+    # CRIAR ambiente virtual
+    # python3 -m venv ambiente
+
+    # ATIVAR ambiente virtual
+    # source /venv/bin/activate
+
+    # DESATIVAR ambiente virtual
+    # source /venv/bin/deactivate
