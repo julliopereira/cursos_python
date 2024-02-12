96b91597cb5386d837dde442fa1cb2cd2f412eb6
julliopereira <julliopereira@gmail.com>
2024-02-11 21:14:25 -0300

    Python Udemy - Até a aula 204 sobre classe

---
+++ 2023_01_Python3_udemy/progr_orientada_objetos_poo/aula133_dict_e_vars_para_atributos_de_instancia.py
@@ -0,0 +1,23 @@
+# __dict__ e vars para atributos de instância
+
+class Pessoa:
+    ano_atual = 2024
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def get_ano_nascimento(self):
+        return Pessoa.ano_atual - self.idade
+
+
+p1 = Pessoa('João', 43)
+# p1.nome = 'EITA'
+# print(p1.idade)
+# print(p1.__dict__)  # load onde está guardado minha entrada (é um dicionário)
+# print(vars(p1))     # vars também mostra a mesma coisa, mesmo conteudo
+# print(p1.outra)
+# print(p1.nome)
+# print(vars(p1))
+print(p1.nome)
+
